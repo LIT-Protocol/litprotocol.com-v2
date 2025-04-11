a44a8dfd5e54e7e3db208c9aab0755df0018308f
alt <84208222+a1ttech@users.noreply.github.com>
2025-04-11 14:47:14 -0700

    Update Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <p>
-        $LITKEY Community Sale starting 3/24. Learn more {' '}
+        Lit v1 is Coming Soon. Stay updated {' '}
         <a href={BANNER_LINK} className={styles.banner__link}>
            here.
         </a>
