09039836581915a231254ce920df5945472ffe6d
alt <84208222+a1ttech@users.noreply.github.com>
2025-04-16 13:49:41 -0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // Resources
 export const DOCS_LINK = 'https://developer.litprotocol.com';
-export const BANNER_LINK = 'https://x.com/LitProtocol';
+export const BANNER_LINK = 'https://x.com/LitProtocol/status/1911850883845149065';
 export const MANIFESTO_LINK =
   'https://spark.litprotocol.com/mass-adoption-of-digital-ownership-and-progressive-self-custody/';
 export const USE_CASES_LINK =
