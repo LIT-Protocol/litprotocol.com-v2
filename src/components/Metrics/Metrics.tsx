92ec49275d977220b8e0689d40484b3b1f071cb6
Jenna Leonard <jleonardweb@gmail.com>
2025-05-23 15:20:38 -0400

    more updates to spacing for mobile wip

--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -39,7 +39,7 @@ function NumberItem({ number, subtext, className }: NumbersProps) {
       }}
     >
       <Text
-        className={`!text-[4rem]/[3.5rem] md:!text-[6rem]/[5.8rem] h-[4.5rem] md:h-[6.5rem] md:mb-[.5rem] 
+        className={`!text-[4rem]/[3.8rem] md:!text-[6rem]/[5.8rem] h-[4.5rem] md:h-[6.5rem] md:mb-[.5rem] 
  ${className}`}
       >
         {number}
