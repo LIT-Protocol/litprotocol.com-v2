49ed3d5c68a2061192b252d3fe1a4abbd3f6e836
Jenna Leonard <jleonardweb@gmail.com>
2025-05-27 15:59:06 -0400

    fixed browser api issue (hopefully) deleted unused files

--- src/components/LandingCta/LandingCta.tsx
+++ src/components/LandingCta/LandingCta.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { DISCORD_LINK, DOCS_LINK, SPARK_LINK } from '@/utils/constants';
 import { Card, Container, Group, Text, Title } from '@mantine/core';
 import React from 'react';
